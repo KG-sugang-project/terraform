06c374a905809dbd36513348a378e246dac3fb26
jylee <ddw9423@gmail.com>
2022-08-01 13:52:12 +0900

    WAF_Modified

--- 37_WAF.tf
+++ 37_WAF.tf
@@ -1,14 +1,30 @@
 resource "aws_wafv2_web_acl" "team_web_acl" {
   name  = "team-web-acl"
   scope = "REGIONAL"
-
   default_action {
     allow {}
   }
-
   rule {
-    name     = "team-rule"
+    name     = "team-rule2"
     priority = 1
+    override_action {
+      count {}
+    }
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "AWS-AWSManagedRulesCommonRuleSet"
+      sampled_requests_enabled   = false
+    }
+  } 
+  rule {
+    name     = "team-rule1"
+    priority = 2
     override_action {
       count {}
     }
@@ -24,6 +40,7 @@ resource "aws_wafv2_web_acl" "team_web_acl" {
       sampled_requests_enabled   = false
     }
   }
+ 
   visibility_config {
     cloudwatch_metrics_enabled = false
     metric_name                = "friendly-metric-name"
