76834f36665ad54702abcbf91b4e6f4da2c13eac
jylee <ddw9423@gmail.com>
2022-08-01 13:15:21 +0900

    Route53_record_Modified

--- 36_Route53_record.tf
+++ 36_Route53_record.tf
@@ -1,7 +1,23 @@
-resource "aws_route53_record" "cname_rec1" {
-  zone_id = aws_route53_zone.Route53_primary.zone_id
-  name    = "www.kguniv.xyz"
-  type    = "CNAME"
-  ttl     = "60"
-  records = [aws_lb.ALB_ex.dns_name] ## 외부용 ALB와 연결
+resource "aws_route53_record" "rec1" {
+  zone_id = "${aws_route53_zone.Route53_primary.zone_id}"
+  name    = "www"
+  type    = "A"
+
+  alias {
+    name                   = "${aws_lb.ALB_ex.dns_name}"
+    zone_id                = "${aws_lb.ALB_ex.zone_id}"
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "rec2" {
+  zone_id = "${aws_route53_zone.Route53_primary.zone_id}"
+  name    = ""
+  type    = "A"
+
+  alias {
+    name                   = "${aws_lb.ALB_ex.dns_name}"
+    zone_id                = "${aws_lb.ALB_ex.zone_id}"
+    evaluate_target_health = true
+  }
 }
